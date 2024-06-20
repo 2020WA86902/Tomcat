a38cbe667b9bd8e2f3bbf4370f8a045abcbebb85
2020WA86902 <167507861+2020WA86902@users.noreply.github.com>
2024-06-20 16:33:45 +0000

    Create Tomcatbuild.java

---
+++ Tomcatbuild.java
@@ -0,0 +1,10 @@
+public class Tomcatbuild
+  {
+    public static void main(String args[])
+    {
+      System.out.println("Welcome to Jenkins");
+      System.out.println("Welcome to DevOps-Jenkins");
+
+    }
+
+  }
